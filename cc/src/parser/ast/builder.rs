345a6337b72d8640d7a755481e728ac68100a4c6
AnisHan <3137687133@qq.com>
2025-09-11 18:27:28 +0800

    正在实现AST

--- cc/src/parser/ast/builder.rs
+++ cc/src/parser/ast/builder.rs
@@ -5,14 +5,14 @@ use crate::parser::span::Span;
 use crate::types::token::Token;
 
 impl DeclaratorChunk {
-    pub fn make_pointer(ptr: Token) -> ParserNode {
-        let span = Span::from_token(&ptr);
-        DeclaratorChunk::Pointer(span).into()
-    }
-
-    pub fn make_array(lbracket: Token, constexpr: Expression, rbracket: Token) -> ParserNode {
-        let span = Span::from_tokens(&[lbracket, rbracket]);
-        DeclaratorChunk::Array(constexpr, span).into()
-    }
+    // pub fn make_pointer(ptr: Token) -> ParserNode {
+    //     let span = Span::from_token(&ptr);
+    //     DeclaratorChunk::Pointer(span).into()
+    // }
+    //
+    // pub fn make_array(lbracket: Token, constexpr: Expression, rbracket: Token) -> ParserNode {
+    //     let span = Span::from_tokens(&[lbracket, rbracket]);
+    //     DeclaratorChunk::Array(constexpr, span).into()
+    // }
 
 }
--- cc/src/parser/ast/temp_nodes.rs
+++ cc/src/parser/ast/temp_nodes.rs
@@ -2,37 +2,59 @@
 //! temp_nodes 是中间节点的类型定义（或者说是未解析节点定义），辅助sema构造
 //!
 
-use crate::parser::ast::ast_nodes::{Expression};
-use crate::parser::ast::parser_node::ParserNode;
 use crate::parser::span::Span;
 use crate::parser::ast::ast_nodes;
 use crate::types::token::Token;
 
-pub struct DeclSpec {
 
+
+pub struct DeclSpec {
+    ty: ast_nodes::Type, // ast节点，可以表示Union Struct Enum
+    declarator: DeclaratorInfo,
 }
 
 /// declarator 的中间结构
 #[derive(Debug, Clone)]
-pub struct Declarator {
-    pub chunks: Vec<DeclaratorChunk>, // 注意是顺序保存
+pub struct DeclaratorInfo {
+    pub chunks: Vec<DeclaratorChunk>, // 注意顺序
 }
 
 /// type qualifier
 #[derive(Debug, Clone)]
-pub enum TypeQualifier {
+pub enum QualifierInfo {
     Const,
     Volatile,
 }
 
 #[derive(Debug, Clone)]
 pub enum DeclaratorChunk {
-    Pointer(Span),
-    Array(Expression, Span),
-    Function(Vec<ast_nodes::Declaration>, Span),
-    OldFunction(Vec<Token>, Span) // 老式函数声明 foo(a, b) int a; int b; {  }
+    Decl(DeclaratorInfo),
+    Pointer(Vec<QualifierInfo>),
+    Array {
+        size: Option<ast_nodes::Expression>, // 大小
+        asm: ArraySizeModifier, // Array类型(Normal, Static, VLA)
+        span: Span
+    },
+    Function {
+        is_variadic: bool,      // 是否是可变参数
+        has_prototype: bool,    // 是否有原型，就是类型
+        params: Vec<ParamInfo>, // 参数列表
+        span: Span
+    },
 }
 
+#[derive(Debug, Clone)]
+pub enum ArraySizeModifier {
+    Normal,     // int a[10];
+    Static,     // int a[] = ...;
+    VLA         // int a[*];  (VLA without size)，C89不支持
+}
+
+#[derive(Debug, Clone)]
+pub enum ParamInfo {
+    Ident(Token),
+    Decl(ast_nodes::Declaration),
+}
 
 
 
