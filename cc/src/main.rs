f9cea6466cf48c3f74ec6037f857af0ebd267d34
AnisHan <88761857+AnisHon@users.noreply.github.com>
2025-09-16 00:15:05 +0800

    大清洗，当然我说的是代码 - 清除无用取引用& - 使用*or_default - 为复杂返回值起别名 - 替换所有的mod.rs模块定义 - 大数组const替换为static - 去除部分无用into - 使用Box让枚举大小接近

--- cc/src/main.rs
+++ cc/src/main.rs
@@ -1,16 +1,8 @@
-use std::io::Cursor;
-use cc::compiler::c_compiler::CCompiler;
-use cc::types::ast::ast_nodes::*;
-use cc::types::ast::decl_info::{CompleteDecl, DeclSpec, Declarator, DeclaratorChunk, ParamInfo, ParamList, TypeQual, TypeSpec};
-use cc::types::ast::parser_node::ParserNode;
-use cc::types::ast::struct_info::{EnumSpec, Enumerator, StructDeclarator, StructKind, StructMember, StructOrUnionSpec};
-use cc::types::span::{Delim, SepList, Span};
-use cc::types::token::Token;
+
 
 fn main() {
     let code = include_str!("../resources/test.c");
     // let compiler = CCompiler::new(Cursor::new(code));
     // compiler.compile();
-    let i = std::mem::size_of::<SepList<Enumerator>>();
-    println!("{}", i);
+
 }
