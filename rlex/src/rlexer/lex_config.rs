ed78c1d73734bb43802632b981a20ba0355e780c
AnisHan <3137687133@qq.com>
2025-09-16 18:00:54 +0800

    WIP: 开始重构Lexer reader的文件解析和代码生成模块

--- rlex/src/rlexer/lex_config.rs
+++ rlex/src/rlexer/lex_config.rs
@@ -1,7 +1,46 @@
+use pest::Parser;
+use pest_derive::Parser;
+
 // 词法分析器 结构
 #[derive(Debug)]
 pub struct LexStruct {
     pub name: String,
     pub regex: String,
     pub skip: bool,
+}
+
+#[derive(Parser)]
+#[grammar = "lexer.pest"]
+pub struct LexGrammar;
+
+pub struct LexConfigParser {
+    input: String,
+}
+
+
+impl LexConfigParser {
+    pub fn new(input: String) -> LexConfigParser {
+        LexConfigParser { input }
+    }
+
+    pub fn parse(self) {
+        let file = LexGrammar::parse(Rule::l_file, self.input.as_str())
+            .unwrap()
+            .next()
+            .unwrap();
+        println!("{:?}", file);
+    }
+
+}
+
+#[test]
+pub fn t() {
+    let input = r#"
+%option noyywrap
+// 注释
+[a-z]+ { println!("identifier"); }
+[0-9]+ { println!("number"); }
+"#;
+
+    LexConfigParser::new(input.to_string()).parse();
 }
