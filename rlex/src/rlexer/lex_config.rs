fe9dcc9d065457790c69d8ce80bf88c6f4945ee1
AnisHan <88761857+AnisHon@users.noreply.github.com>
2025-09-17 00:08:07 +0800

    WPI: 正在重构Lex配置文件，提供pest解析文法

--- rlex/src/rlexer/lex_config.rs
+++ rlex/src/rlexer/lex_config.rs
@@ -1,3 +1,4 @@
+use pest::iterators::{Pair, Pairs};
 use pest::Parser;
 use pest_derive::Parser;
 
@@ -9,26 +10,110 @@ pub struct LexStruct {
     pub skip: bool,
 }
 
+///
+/// # Members
+/// - `decl_code`: 头部声明代码
+/// - `decls`: 声明配置
+/// - `rules`: lex规则
+/// - `user_code`: 用户代码
+#[derive(Debug)]
+pub struct LexConfig {
+    decl_code: String,
+    decls: Vec<LexDecl>,
+    rules: Vec<LexRule>,
+    user_code: String,
+}
+
+#[derive(Debug)]
+pub enum LexDecl {
+    Option(String)
+}
+
+#[derive(Debug)]
+pub struct LexRule {
+    regex: String,
+    action: Option<String>,
+}
+
 #[derive(Parser)]
 #[grammar = "lexer.pest"]
 pub struct LexGrammar;
 
 pub struct LexConfigParser {
     input: String,
+    decl_code: String,
+    decls: Vec<LexDecl>,
+    rules: Vec<LexConfigRule>,
+    user_code: String,
+}
+
+pub enum PatternType {
+    Unquoted,
+    Quoted,
+}
+
+
+pub struct LexConfigRule {
+    pattern: String,
+    pattern_type: PatternType,
+    action: Option<String>,
 }
 
 
 impl LexConfigParser {
     pub fn new(input: String) -> LexConfigParser {
-        LexConfigParser { input }
+        LexConfigParser {
+            input,
+            decl_code: String::new(),
+            decls: Vec::new(),
+            rules: Vec::new(),
+            user_code: String::new()
+        }
     }
 
-    pub fn parse(self) {
-        let file = LexGrammar::parse(Rule::l_file, self.input.as_str())
+    pub fn parse(mut self) {
+
+        let input = self.input.clone();
+
+        let file = LexGrammar::parse(Rule::file, input.as_str())
             .unwrap()
             .next()
             .unwrap();
-        println!("{:?}", file);
+
+        for x in file.into_inner() {
+            match x.as_rule() {
+                Rule::decl_code => {
+                    let user_code: Vec<char> = x.as_str().chars().collect();
+                    let user_code: String = user_code[2..user_code.len() - 2].iter().collect();
+                    println!("{}", user_code)
+                },
+                Rule::decls => { /* 目前无用忽略 */ },
+                Rule::rules => self.parse_rules(x.into_inner()),
+                Rule::user_code => println!("{}", x.as_str()),
+                Rule::EOI => {}
+                _ => unreachable!()
+            }
+        }
+        // println!("{:?}", file);
+    }
+
+    pub fn parse_rules(&mut self, rules: Pairs<Rule>) {
+        for rule_decl in rules {
+            assert_eq!(rule_decl.as_rule(), Rule::rule_decl);
+
+            let mut regex: Option<String> = None;
+            let mut regex_acton: Option<String> = None;
+
+            for pair in rule_decl.into_inner() {
+                match pair.as_rule() {
+                    Rule::pattern => regex = Some(),
+                    Rule::action => regex_acton = Some(pair.as_str().to_string()),
+                }
+
+
+            }
+
+        }
     }
 
 }
@@ -36,11 +121,21 @@ impl LexConfigParser {
 #[test]
 pub fn t() {
     let input = r#"
+%{
+#include <stdio.h>
+%}
+
 %option noyywrap
-// 注释
-[a-z]+ { println!("identifier"); }
-[0-9]+ { println!("number"); }
+
+%%
+[a-z]+    { return IDENT; }
+"+"       { return PLUS; }
+[0-9]+    { return NUMBER; }
+%%
+
+int main() { yylex(); }
 "#;
+
 
     LexConfigParser::new(input.to_string()).parse();
 }
